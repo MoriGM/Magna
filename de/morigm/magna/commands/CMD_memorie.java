859d2259524b1e7868acb95268399929486b1f53
MoriGM <email@mail>
2018-06-09 17:27:54 +0200

    Added GIGABYTE MEGABYTE KILOBYTE TO Command memorie

--- de/morigm/magna/commands/CMD_memorie.java
+++ de/morigm/magna/commands/CMD_memorie.java
@@ -2,6 +2,7 @@ package de.morigm.magna.commands;
 
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 
 import de.morigm.magna.Main;
 import de.morigm.magna.api.helper.CommandHelper;
@@ -15,9 +16,37 @@ public class CMD_memorie extends CommandHelper
 	{
 		if(com.hasPermission(getPermission("memorie")))
 		{
-			com.sendMessage(Chat.prefix + "Used:" + Main.getInstance().getMemorieManager().getUsedMemorie(MemorieType.MEGABYTE) + "MB");
-			com.sendMessage(Chat.prefix + "Free:" + Main.getInstance().getMemorieManager().getFreeMemorie(MemorieType.MEGABYTE) + "MB");
-			com.sendMessage(Chat.prefix + "Total:" + Main.getInstance().getMemorieManager().getTotalMemorie(MemorieType.MEGABYTE) + "MB");
+			if(args.length >= 1)
+			{
+				if(args[0].equalsIgnoreCase("GIGABYTE") || args[0].equalsIgnoreCase("GB"))
+				{
+					com.sendMessage(Chat.prefix + "Used:" + Main.getInstance().getMemorieManager().getUsedMemorie(MemorieType.GIGABYTE) + "GB");
+					com.sendMessage(Chat.prefix + "Free:" + Main.getInstance().getMemorieManager().getFreeMemorie(MemorieType.GIGABYTE) + "GB");
+					com.sendMessage(Chat.prefix + "Total:" + Main.getInstance().getMemorieManager().getTotalMemorie(MemorieType.GIGABYTE) + "GB");
+				}
+				else
+				if(args[0].equalsIgnoreCase("MEGABYTE") || args[0].equalsIgnoreCase("MB"))
+				{
+					com.sendMessage(Chat.prefix + "Used:" + Main.getInstance().getMemorieManager().getUsedMemorie(MemorieType.MEGABYTE) + "MB");
+					com.sendMessage(Chat.prefix + "Free:" + Main.getInstance().getMemorieManager().getFreeMemorie(MemorieType.MEGABYTE) + "MB");
+					com.sendMessage(Chat.prefix + "Total:" + Main.getInstance().getMemorieManager().getTotalMemorie(MemorieType.MEGABYTE) + "MB");
+				}
+				else
+				if(args[0].equalsIgnoreCase("KILOBYTE") || args[0].equalsIgnoreCase("KB"))
+				{
+					com.sendMessage(Chat.prefix + "Used:" + Main.getInstance().getMemorieManager().getUsedMemorie(MemorieType.KILOBYTE) + "KB");
+					com.sendMessage(Chat.prefix + "Free:" + Main.getInstance().getMemorieManager().getFreeMemorie(MemorieType.KILOBYTE) + "KB");
+					com.sendMessage(Chat.prefix + "Total:" + Main.getInstance().getMemorieManager().getTotalMemorie(MemorieType.KILOBYTE) + "KB");
+				}
+				else
+					com.sendMessage(Chat.prefix + (com instanceof Player ? "/" : "") + "memorie <GIGABYTE,MEGABYTE,KILOBYTE>");
+			}
+			else
+			{
+				com.sendMessage(Chat.prefix + "Used:" + Main.getInstance().getMemorieManager().getUsedMemorie(MemorieType.MEGABYTE) + "MB");
+				com.sendMessage(Chat.prefix + "Free:" + Main.getInstance().getMemorieManager().getFreeMemorie(MemorieType.MEGABYTE) + "MB");
+				com.sendMessage(Chat.prefix + "Total:" + Main.getInstance().getMemorieManager().getTotalMemorie(MemorieType.MEGABYTE) + "MB");
+			}
 		}
 		else
 			com.sendMessage(Chat.no_permission);
