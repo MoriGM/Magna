d0f58c531d06d29da558bb8cf7daf72c3444b283
MoriGM <30366747+MoriGM@users.noreply.github.com>
2018-07-14 18:15:18 +0200

    Added Command BanIps

---
+++ src/de/morigm/magna/commands/BanIps.java
@@ -0,0 +1,32 @@
+package de.morigm.magna.commands;
+
+import org.bukkit.BanEntry;
+import org.bukkit.BanList;
+import org.bukkit.Bukkit;
+import org.bukkit.BanList.Type;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+
+import de.morigm.magna.api.helper.CommandHelper;
+import de.morigm.magna.chat.Chat;
+
+public class BanIps extends CommandHelper 
+{
+
+	@Override
+	public boolean onCommand(CommandSender com, Command cmd, String label, String[] args) 
+	{
+		if(com.hasPermission(getPermission("banips")))
+		{
+			String bans = "";
+			BanList list = Bukkit.getBanList(Type.IP);
+			for(BanEntry ban : list.getBanEntries())
+				bans += ban.getTarget() + " ";
+			com.sendMessage("BanIps:" + bans);
+		}
+		else
+			com.sendMessage(Chat.prefix + Chat.no_permission);
+		return false;
+	}
+
+}
