04d3d9e02bba48bf7d3f4b13bb1effe81053fad1
MoriGM <30366747+MoriGM@users.noreply.github.com>
2018-07-16 10:37:46 +0200

    Added First Junit Test

---
+++ Magna/src/test/java/de/morigm/magna/test/StartTest.java
@@ -0,0 +1,34 @@
+package de.morigm.magna.test;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class StartTest 
+{
+
+	@Test
+	public void PluginYMLExistsTest()
+	{
+		assertTrue(ClassLoader.getSystemResourceAsStream("plugin.yml") != null);
+	}
+	
+	@Test
+	public void ENYMLExistsTest()
+	{
+		assertTrue(ClassLoader.getSystemResourceAsStream("en-en.yml") != null);
+	}
+	
+	@Test
+	public void DEYMLExistsTest()
+	{
+		assertTrue(ClassLoader.getSystemResourceAsStream("de-de.yml") != null);
+	}
+	
+	@Test
+	public void PermissionYMLExistsTest()
+	{
+		assertTrue(ClassLoader.getSystemResourceAsStream("Permission.yml") != null);
+	}
+	
+}
