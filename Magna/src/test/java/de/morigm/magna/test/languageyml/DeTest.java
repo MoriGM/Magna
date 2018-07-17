864443d95ae717abf47a862c55c03dd5b42c5766
MoriGM <30366747+MoriGM@users.noreply.github.com>
2018-07-17 11:43:56 +0200

    Added Junit Language Test system to Magna

---
+++ Magna/src/test/java/de/morigm/magna/test/languageyml/DeTest.java
@@ -0,0 +1,44 @@
+package de.morigm.magna.test.languageyml;
+
+import static org.junit.Assert.assertTrue;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Properties;
+
+import org.junit.Test;
+
+import de.morigm.magna.api.Magna;
+import de.morigm.magna.loader.PluginLoader;
+
+public class DeTest 
+{
+	public class CommandLoader extends PluginLoader
+	{
+		private boolean isactive = false; 
+		
+		@Override
+		public void registerCommands() 
+		{
+			if(!isactive)
+				super.registerCommands();
+			isactive = true;
+		}
+	}
+	
+	@Test
+	public void testDeYml() throws IOException
+	{
+		CommandLoader loader = new CommandLoader();
+		loader.registerCommands();
+		List<String> translations = Magna.getCommandUtil().getTranslations();
+		Properties prop = new Properties();
+		prop.load(ClassLoader.getSystemResourceAsStream("de-de.yml"));
+		for(String s : translations)
+		{
+			System.out.println("Test German Translation: " + s);
+			assertTrue(prop.containsKey(s));
+		}
+	}
+
+}
---
+++ Magna/src/test/java/de/morigm/magna/test/languageyml/EnTest.java
@@ -0,0 +1,43 @@
+package de.morigm.magna.test.languageyml;
+
+import static org.junit.Assert.assertTrue;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Properties;
+
+import org.junit.Test;
+
+import de.morigm.magna.api.Magna;
+import de.morigm.magna.loader.PluginLoader;
+
+public class EnTest 
+{
+	public class CommandLoader extends PluginLoader
+	{
+		private boolean isactive = false; 
+		
+		@Override
+		public void registerCommands() 
+		{
+			if(!isactive)
+				super.registerCommands();
+			isactive = true;
+		}
+	}
+	
+	@Test
+	public void testEnYml() throws IOException
+	{
+		CommandLoader loader = new CommandLoader();
+		loader.registerCommands();
+		List<String> translations = Magna.getCommandUtil().getTranslations();
+		Properties prop = new Properties();
+		prop.load(ClassLoader.getSystemResourceAsStream("en-en.yml"));
+		for(String s : translations)
+		{
+			System.out.println("Test English Translation: " + s);
+			assertTrue(prop.containsKey(s));
+		}
+	}
+}
