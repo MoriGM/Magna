4c5a2bfd44aa3625dc64349717bdf49a0e1a41e5
MoriGM <30366747+MoriGM@users.noreply.github.com>
2024-01-12 21:52:14 +0100

    Use Kotlin

--- Magna/src/main/java/de/morigm/magna/api/chat/ChatColor.kt
+++ Magna/src/main/java/de/morigm/magna/api/chat/ChatColor.kt
@@ -1,35 +1,32 @@
-package de.morigm.magna.api.chat;
+package de.morigm.magna.api.chat
 
-public enum ChatColor {
-    BLACK('0', "black"), DARK_BLUE('1', "dark_blue"), DARK_GREEN('2', "dark_green"), DARK_AQUA('3', "dark_aqua"),
-    DARK_RED('4', "dark_red"), DARK_PURPLE('5', "dark_purple"), GOLD('6', "gold"), GRAY('7', "gray"),
-    DARK_GRAY('8', "dark_gray"), BLUE('9', "blue"), GREEN('a', "green"), AQUA('b', "aqua"), RED('c', "red"),
-    LIGHT_PURPLE('d', "light_purple"), YELLOW('e', "yellow"), WHITE('f', "white"), MAGIC('k', "obfuscated"),
-    BOLD('l', "bold"), STRIKETHROUGH('m', "strikethrough"), UNDERLINE('n', "underline"), ITALIC('o', "italic"),
-    RESET('r', "reset"), COLOR_CHAR('§', "");
+enum class ChatColor(val colorChar: Char) {
 
-    private final String color_name;
-    private final char color_char;
-    private final String to_string;
+    BLACK('0'),
+    DARK_BLUE('1'),
+    DARK_GREEN('2'),
+    DARK_AQUA('3'),
+    DARK_RED('4'),
+    DARK_PURPLE('5'),
+    GOLD('6'),
+    GRAY('7'),
+    DARK_GRAY('8'),
+    BLUE('9'),
+    GREEN('a'),
+    AQUA('b'),
+    RED('c'),
+    LIGHT_PURPLE('d'),
+    YELLOW('e'),
+    WHITE('f'),
+    MAGIC('k'),
+    BOLD('l'),
+    STRIKETHROUGH('m'),
+    UNDERLINE('n'),
+    ITALIC('o'),
+    RESET('r'),
+    COLOR_CHAR('§');
 
-    ChatColor(char c, String color_code) {
-        this.color_char = c;
-        this.color_name = color_code;
-        char code_char = '§';
-        this.to_string = new String(new char[]{code_char, color_char});
+    override fun toString(): String {
+        return String(charArrayOf('§', colorChar))
     }
-
-    public String getColorName() {
-        return color_name;
-    }
-
-    public char getColorChar() {
-        return color_char;
-    }
-
-    @Override
-    public String toString() {
-        return this.to_string;
-    }
-
 }
